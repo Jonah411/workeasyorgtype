f623418cc3f0461d1eea49a9684bed5e25fba6f5
Jonah-john-thangadurai <55949424+Jonah411@users.noreply.github.com>
2024-10-15 21:21:55 +0530

    update Code

--- src/common/ConstaltsVariables.js
+++ src/common/ConstaltsVariables.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://ewserver.onrender.com/app";
+export const BASE_URL = "https://ewserver.onrender.com/app";
 
 // export const BASE_URL = "http://localhost:5001/app";
 
-export const BASE_URL = "http://192.168.1.6:5001/app";
+// export const BASE_URL = "http://192.168.1.6:5001/app";
